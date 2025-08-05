45ed774ece80c50ac2b9da0f71b0ae4d5214e74e
m4rkdv <yo.mark406@gmail.com>
2025-08-05 17:34:06 -0300

    refactor(system-config): improve getCurrent to return existing config or create default

--- apps/backend/src/infrastructure/repositories/system-config.impl.ts
+++ apps/backend/src/infrastructure/repositories/system-config.impl.ts
@@ -4,13 +4,17 @@ import { SystemConfigModel } from '../database/mongo/models/system-config.model'
 
 export class MongoSystemConfigRepository implements SystemConfigRepository {
     async getCurrent(): Promise<SystemConfig> {
-        const defaultConfig = new SystemConfigModel({
-            basePrice: 28000,
-            gracePeriodDays: 10,
-            suspensionMonths: 3,
-            updatedBy: 'system'
-        });
-        const config = await defaultConfig.save();
+        let config = await SystemConfigModel.findOne();
+
+        if (!config) {
+            config = new SystemConfigModel({
+                basePrice: 15000,
+                gracePeriodDays: 10,
+                suspensionMonths: 3,
+                updatedBy: 'system'
+            });
+            await config.save();
+        }
 
         return {
             basePrice: config.basePrice,
