f7e8d5a95b3d8f1308fedc7b229c65cffbc532b1
m4rkdv <yo.mark406@gmail.com>
2025-08-05 09:55:41 -0300

    test(auth.middleware):add handle 401 error whit missing header

--- apps/backend/src/presentation/auth/auth.middleware.test.ts
+++ apps/backend/src/presentation/auth/auth.middleware.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, Mock, test, vi } from 'vitest';
 import { JwtService } from '../services/jwt.service';
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { authenticateToken, AuthRequest, JwtPayload } from './auth.middleware';
 
 vi.mock('../services/jwt.service', () => ({
@@ -20,12 +20,16 @@ describe('authenticateToken middleware', () => {
     let mockRes: Partial<Response>;
     let mockJson: Mock;
     let mockStatus: Mock;
+    let mockNext: NextFunction;
 
     beforeEach(() => {
         mockJson = vi.fn();
-        mockStatus = vi.fn().mockReturnValue({ json: mockJson });        
+        mockStatus = vi.fn().mockReturnValue({ json: mockJson });
+
         mockReq = { headers: {} };
         mockRes = { json: mockJson, status: mockStatus };
+        mockNext = vi.fn();
+
         (JwtService.validateToken as Mock).mockReset();
     });
 
@@ -35,10 +39,19 @@ describe('authenticateToken middleware', () => {
         };
         (JwtService.validateToken as Mock).mockRejectedValue(new Error('Invalid token'));
 
-        await authenticateToken(mockReq as AuthRequest, mockRes as Response);
+        await authenticateToken(mockReq as AuthRequest, mockRes as Response, mockNext);
 
         expect(mockStatus).toHaveBeenCalledWith(403);
         expect(mockJson).toHaveBeenCalledWith({ error: 'Token validation failed' });
     });
 
+    test("missing Authorization header returns 401 with error message", async () => {
+        mockReq.headers = {};
+
+        await authenticateToken(mockReq as AuthRequest, mockRes as Response, mockNext);
+
+        expect(mockStatus).toHaveBeenCalledWith(401);
+        expect(mockJson).toHaveBeenCalledWith({ error: 'Access token required' });
+        expect(mockNext).not.toHaveBeenCalled();
+    });
 });
