59216cf7aa11516b2131239bffa2db0cf316f75b
m4rkdv <yo.mark406@gmail.com>
2025-08-06 11:57:28 -0300

    refactor(routes): implement AppRoutes with dependency injection support

--- apps/backend/src/presentation/routes.ts
+++ apps/backend/src/presentation/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
+import { Dependencies } from "./dependencies";
 
 export class AppRoutes {
+    constructor(private readonly dependencies: Dependencies) { }
 
-
-    get routes():Router{
-
+    get routes(): Router {
         const appRouter = Router();
-        const authRouter = new AuthRoutes;
-        
+        const authRouter = new AuthRoutes(this.dependencies);
+
         appRouter.use('/api/auth', authRouter.routes)
 
         return appRouter;
