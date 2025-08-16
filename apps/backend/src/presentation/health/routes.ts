406b0228172517a54dbdac6f4873b0e2275912ef
m4rkdv <yo.mark406@gmail.com>
2025-08-16 05:34:38 -0300

    feat(health): implement health check endpoints in backend for Docker

---
+++ apps/backend/src/presentation/health/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { HealthController } from './health.controller';
+
+export class HealthRoutes {
+  get routes(): Router {
+    const router = Router();
+    const controller = new HealthController();
+
+    router.get('/health', controller.checkHealth);
+
+    return router;
+  }
+}
